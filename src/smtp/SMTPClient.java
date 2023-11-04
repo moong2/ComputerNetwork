d3929616e9ba125013252056225ea25b56949c9f
Boeun <boeun0904@naver.com>
2023-11-04 16:21:12 +0900

    feat: attach file

--- src/smtp/SMTPClient.java
+++ src/smtp/SMTPClient.java
@@ -2,11 +2,13 @@ package smtp;
 
 import java.io.*;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import socket.NaverSocket;
 import socket.SocketHelper;
+import java.util.UUID;
 
 public class SMTPClient {
     public static void main(String[] args) {
@@ -24,25 +26,25 @@ public class SMTPClient {
             }
 
             boolean isTLS = false;
-//            for (String response : responses) {
-//                if (response.contains("250-STARTTLS")) {
-//                    System.out.println("here!");
-//                    socket.sendRequest("STARTTLS");
-//                    response = socket.readResponse();
-//                    System.out.println("response = " + response);
-//                    socket.upgradeToSSL();
-//
-//                    isTLS = true;
-//                    break;
-//                }
-//            }
+            for (String response : responses) {
+                if (response.contains("250-STARTTLS")) {
+                    System.out.println("here!");
+                    socket.sendRequest("STARTTLS");
+                    response = socket.readResponse();
+                    System.out.println("response = " + response);
+                    socket.upgradeToSSL();
 
-            if (!isTLS) {
-                socket.close();
-                socket.FromSSL();
-                socket.upgradeToSSL();
-                System.out.println("socket.readResponse() = " + socket.readResponse());
+                    isTLS = true;
+                    break;
+                }
             }
+
+//            if (!isTLS) {
+//                socket.close();
+//                socket.FromSSL();
+//                socket.upgradeToSSL();
+//                System.out.println("socket.readResponse() = " + socket.readResponse());
+//            }
 
             System.out.println("AUTH LOGIN");
             socket.sendRequest("AUTH LOGIN");
@@ -76,14 +78,39 @@ public class SMTPClient {
             System.out.println(socket.readResponse());
             System.out.println("===========");
 
+            String boundary = "abcdxyz";
+            String attachmentFilePath = "C:\\Users\\Boeun\\Downloads\\cat.jpg";
+
             socket.sendRequest("From: Boeun <boeun0904@naver.com>");
-            socket.sendRequest("Subject: 넌 이름이 뭐야");
+            socket.sendRequest("To: <h.boeunn@gmail.com>");
+            socket.sendRequest("Subject: 제목입니다");
+            socket.sendRequest("MIME-Version: 1.0");
+            socket.sendRequest("Content-Type: multipart/mixed; boundary=\"" + boundary + "\"");
+            socket.sendRequest("");
+
+            socket.sendRequest("--" + boundary);
             socket.sendRequest("Content-Type: text/plain; charset=utf-8");
+            socket.sendRequest("Content-Transfer-Encoding: 7bit");
+            socket.sendRequest("");
             socket.sendRequest("hello");
             socket.sendRequest("this is SMTP test");
             socket.sendRequest("can you hear me?");
             socket.sendRequest("from Boeun");
+
+            socket.sendRequest("--" + boundary);
+            socket.sendRequest("Content-Type: application/octet-stream; name=\"" + new File(attachmentFilePath).getName() + "\"");
+            socket.sendRequest("Content-Disposition: attachment; filename=\"" + new File(attachmentFilePath).getName() + "\"");
+            socket.sendRequest("Content-Transfer-Encoding: base64");
+            socket.sendRequest("");
+
+            byte[] attachmentBytes = Files.readAllBytes(new File(attachmentFilePath).toPath());
+            String attachmentBase64 = Base64.getEncoder().encodeToString(attachmentBytes);
+            socket.sendRequest(attachmentBase64);
+            socket.sendRequest("");
+
+            socket.sendRequest("--" + boundary + "--");
             socket.sendRequest(".");
+
             System.out.println(socket.readResponse());
             System.out.println("===========");
 
