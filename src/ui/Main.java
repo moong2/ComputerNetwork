692c2c285be8653f382adc84d00f37436da0985a
Boeun <boeun0904@naver.com>
2023-11-05 13:34:12 +0900

    feat: quit process when clicking x button

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,6 +2,7 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Main extends JFrame{
     private CardLayout card=new CardLayout();
@@ -11,6 +12,12 @@ public class Main extends JFrame{
         add("Mail_gui",mail);
         setBounds(300,100,544,620);
         setVisible(true);
+
+        this.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
     public static void main(String[] args) {
         new Main();
